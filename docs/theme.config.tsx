75d664e003ed2d5d144c0dd8facce1faf185c30e
NicolasQueiroga <n.macielqueiroga@gmail.com>
2023-10-26 17:23:25 -0300

    update

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Hybrid Quantum Neural Network</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/NicolasQueiroga/qnn-project',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
